69bde329b2151aed40b92d154e07d16784a3e96c
alocinny <alocinnygame@gmail.com>
2025-05-27 22:49:22 -0300

    adicionado script da segunda barra de progresso

---
+++ scripts/hotbar.gd
@@ -0,0 +1,15 @@
+extends Control
+
+func can_drop_data(position, data):
+	return data != null
+	
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
--- scripts/matchStatus2.gd
+++ scripts/matchStatus2.gd
@@ -1,11 +1,11 @@
 extends CanvasLayer
 
 @onready var matchStatus: TextureProgressBar = $Control/HBoxContainer/matchStatus
-@onready var statusLabel: Label = $Control/HBoxContainer/NinePatchRect/statuslabel
+@onready var statusLabel: Label = $Control/HBoxContainer/NinePatchRect/Label
 
 func _ready():
 	# pra testar
-	update_progressBar(60.0)
+	update_progressBar(45)
 
 func update_progressBar(valor: float) -> void:
 	valor = clamp(valor, matchStatus.min_value, matchStatus.max_value)
@@ -14,3 +14,7 @@ func update_progressBar(valor: float) -> void:
 	
 	var porcentagem = int((valor - matchStatus.min_value) / (matchStatus.max_value - matchStatus.min_value)*100.0)
 	statusLabel.text = "%d%%" % porcentagem
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
