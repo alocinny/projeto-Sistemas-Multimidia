c399c227e914073c879ac0444f39f30649156da8
pedromonteir1111 <pmfm2004@gmail.com>
2025-05-30 00:41:14 -0300

    padding

--- scripts/tile.gd
+++ scripts/tile.gd
@@ -1,7 +1,5 @@
 extends Node2D
 
-#TODO: ajustar logica de padding, ta estranho
-
 const Biome = preload("res://scripts/enums.gd").Biome
 const EdgeType = preload("res://scripts/enums.gd").EdgeType
 const SIZE_IMG = 512
@@ -9,6 +7,7 @@ const HALF_IMG = SIZE_IMG/2
 const QUARTER_IMG = SIZE_IMG/4
 const PADDING = 50
 var edges = []
+var elements = [[],[],[],[]]
 @onready var base = $Base
 
 func _ready():
@@ -17,25 +16,28 @@ func _ready():
 func initialize_tile(biome, trees, bushes):
 	var tree_prob
 	var tree_density
-	# densidade de arbustos eh fixa
+	var bush_density
 	
 	match biome:
 		Biome.PAMPAS: 
 			tree_prob = 0.50
 			tree_density = 1
+			bush_density = 2
 		Biome.CAATINGA: 
 			tree_prob = 0.10
 			tree_density = 1
+			bush_density = 1
 		
 	for i in 4:	
 		if randf() < tree_prob:
 			edges.append(EdgeType.TREE)
 			place_elements(trees, tree_density, i)
-		else:
+		else: 
 			edges.append(EdgeType.BUSH)
-			place_elements(bushes, 2, i)
+			place_elements(bushes, randi_range(0, bush_density), i)
 			
-	
+	print("edges: ", edges)
+	print("bushes: ", bushes.size())
 
 func place_elements(textures, density, edge_index):
 	var a # esquerda ou direita do eixo y
@@ -48,31 +50,32 @@ func place_elements(textures, density, edge_index):
 	match edge_index:
 		0:
 			a = 1
-			b = -1
+			b = 1
 			m = 0.5
 			n = -1 * QUARTER_IMG
 		1:
 			a = 1
-			b = 1
+			b = -1
 			m = -0.5
 			n = 1 * QUARTER_IMG
 		2:
 			a = -1
-			b = 1
+			b = -1
 			m = 0.5
 			n = 1 * QUARTER_IMG
 		3:
 			a = -1
-			b = -1
+			b = 1
 			m = -0.5
 			n = -1 * QUARTER_IMG
 	
 	for i in density:
-		var x = randi_range(PADDING*a, (HALF_IMG - PADDING)*a)
-		var y = randi_range(PADDING*b, m*x + n - PADDING*b)
+		var x = randi_range(0, (HALF_IMG - PADDING)*a)
+		var y = randi_range(0, m*x + n + PADDING*b)
 		
 		var sprite = Sprite2D.new()
-		sprite.texture = textures[0]
-		sprite.position = Vector2(x, y - HALF_IMG)
+		sprite.texture = textures[randi_range(0, textures.size() - 1)]
+		sprite.position = Vector2(x, y - HALF_IMG + 12)
 		sprite.z_index = sprite.position.y + HALF_IMG + QUARTER_IMG + 1
+		elements[edge_index].append(sprite)
 		add_child(sprite)
