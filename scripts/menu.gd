0bf8e4aac61de3d6ddad1a224c41b745fd3a9c95
alocinny <alocinnygame@gmail.com>
2025-06-03 22:05:11 -0300

    linkando menus

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -3,7 +3,7 @@ extends Control
 
 func _on_start_pressed() -> void:
 	print("Start pressed") #colocar a cena do jogo quando fizer
-	#get_tree().change_scene_to_file(caminho)
+	get_tree().change_scene_to_file("res://scenes/lvl_select/caatinga.tscn")
 
 
 func _on_options_pressed() -> void:
