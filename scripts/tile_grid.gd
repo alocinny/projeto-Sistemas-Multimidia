b41d66dcb3110623f546bbf202fcbfc8466a4a74
pedromonteir1111 <pmfm2004@gmail.com>
2025-06-10 01:34:59 -0300

    contando grupos de rios e florestas

--- scripts/tile_grid.gd
+++ scripts/tile_grid.gd
@@ -27,6 +27,10 @@ func place_tile(pos, tile):
 	tiles[pos] = tile
 	update_vacant()
 	
+	print("Rios: ",get_group_sizes(EdgeType.RIVER))
+	print("Florestas: ",get_group_sizes(EdgeType.TREE))
+	print("")
+	
 func update_vacant():
 	vacant_spots.clear()
 	forbidden_spots.clear()
@@ -46,11 +50,87 @@ func update_vacant():
 	
 func can_place_tile(dir, tile):
 	if tile.edges[direction[dir]] != GameState.dragged_tile.edges[opposite_direction[dir]]:
-		print("entrou")
 		if tile.edges[direction[dir]] == EdgeType.RIVER or GameState.dragged_tile.edges[opposite_direction[dir]] == EdgeType.RIVER:
 			return false
 	
 	return true
+	
+func get_group_sizes(type):
+	var visited = {}
+	var group_sizes = []
+
+	for pos in tiles.keys():
+		if visited.has(pos):
+			continue
+
+		var tile = tiles[pos]
+		if tile == null:
+			continue
+
+		var has_type = false
+		for edge in tile.edges:
+			if edge == type:
+				has_type = true
+				break
+		if not has_type:
+			continue
+
+		var size = flood_fill(pos, visited, type)
+		group_sizes.append(size)
+
+	return group_sizes
+
+
+func flood_fill(start_pos, visited, type):
+	if visited.has(start_pos):
+		return 0
+
+	var stack = [start_pos]
+	var count = 0
+
+	while stack.size() > 0:
+		var current = stack.pop_back()
+		if visited.has(current):
+			continue
+		visited[current] = true
+
+		var tile = tiles.get(current)
+		if tile == null:
+			continue
+
+		var has_type = false
+		for edge in tile.edges:
+			if edge == type:
+				has_type = true
+				break
+		if not has_type:
+			continue
+
+		count += 1
+		if type == EdgeType.RIVER:
+			for dir in [Vector2i.UP, Vector2i.DOWN, Vector2i.LEFT, Vector2i.RIGHT]:
+				var i = direction[dir]
+				if tile.edges[i] == type:
+					var neighbor_pos = current + dir
+					var neighbor = tiles.get(neighbor_pos)
+					if neighbor == null or visited.has(neighbor_pos):
+						continue
+
+					var opposite_i = opposite_direction[dir]
+					if neighbor.edges[opposite_i] == type:
+						stack.append(neighbor_pos)
+						
+		elif type == EdgeType.TREE:
+			for dir in [Vector2i.UP, Vector2i.DOWN, Vector2i.LEFT, Vector2i.RIGHT]:
+				var neighbor_pos = current + dir
+				var neighbor = tiles.get(neighbor_pos)
+				if neighbor == null or visited.has(neighbor_pos):
+					continue
+
+				if EdgeType.TREE in neighbor.edges:
+					stack.append(neighbor_pos)
+
+	return count
 
 func grid_to_global_coords(pos):
 	@warning_ignore("integer_division")
